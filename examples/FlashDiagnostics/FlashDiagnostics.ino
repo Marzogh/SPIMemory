4925397b525f25dd1dab61c570ba74572bb82ee4
Prajwal Bhattaram <Marzogh@users.noreply.github.com>
2018-05-02 15:53:35 +1000

    Reduced memory footprint of Diagnostics.ino and updated information re: libary name change

--- examples/FlashDiagnostics/FlashDiagnostics.ino
+++ examples/FlashDiagnostics/FlashDiagnostics.ino
@@ -1,15 +1,15 @@
 /*
   |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
   |                                                             FlashDiagnostics.ino                                                              |
-  |                                                               SPIFlash library                                                                |
-  |                                                                   v 3.1.0                                                                     |
+  |                                                               SPIMemory library                                                                |
+  |                                                                   v 3.2.0                                                                     |
   |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
   |                                                                    Marzogh                                                                    |
-  |                                                                  04.03.2018                                                                   |
+  |                                                                  02.05.2018                                                                   |
   |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
   |                                                                                                                                               |
   |                                  For a full diagnostics rundown - with error codes and details of the errors                                  |
-  |                                 uncomment #define RUNDIAGNOSTIC in SPIFlash.h in the library before compiling                                 |
+  |                                 uncomment #define RUNDIAGNOSTIC in SPIMemory.h in the library before compiling                                 |
   |                                             and loading this application onto your Arduino.                                                   |
   |                                                                                                                                               |
   |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
@@ -53,49 +53,88 @@ void setup() {
   }
   Serial.println();
   randomSeed(analogRead(RANDPIN));
-  //while (!
-  flash.begin();/*) {
-    delay(1000);
-  }*/
+  flash.begin();
   //To use a custom flash memory size (if using memory from manufacturers not officially supported by the library) - declare a size variable according to the list in defines.h
   //flash.begin(MB(1));
-  Serial.println();
-  Serial.println();
 
   if (getID()) {
-    eraseChipTest();
-    eraseSectionTest();
-    eraseBlock64KTest();
-    eraseBlock32KTest();
-    eraseSectorTest();
-    Serial.println();
-
-#if defined(ARDUINO_ARCH_SAM) || defined(ARDUINO_ARCH_ESP8266)
-    delay(10);
+    
+    printLine();
+    printTab(7);
+    Serial.print("Testing library code");
+    printLine();
+    printTab(3);
+    Serial.print("Function");
+    printTab(2);
+    Serial.print("Data I/O test");
+    printTab(3);
+    Serial.print("     Runtime");
+    printLine();
+    
     powerDownTest();
+    Serial.println();
     powerUpTest();
     Serial.println();
-#endif
+    
+    Serial.println();
+    
+    eraseChipTest();
+    Serial.println();
+    eraseSectionTest();
+    Serial.println();
+    eraseBlock64KTest();
+    Serial.println();
+    eraseBlock32KTest();
+    Serial.println();
+    eraseSectorTest();
+    //Serial.println();
 
+
+    /* #if defined(ARDUINO_ARCH_SAM) || defined(ARDUINO_ARCH_ESP8266)
+        delay(10);
+        powerDownTest();
+        powerUpTest();
+        Serial.println();
+      #endif */
+    printLine();
+    printTab(3);
+    Serial.print("Function");
+    printTab(2);
+    Serial.print("Data I/O test");
+    printTab(1);
+    Serial.print("      Write time");
+    printTab(1);
+    Serial.print("      Read time");
+    printLine();
+    
     byteTest();
+    Serial.println();
     charTest();
+    Serial.println();
     wordTest();
+    Serial.println();
     shortTest();
+    Serial.println();
     uLongTest();
-#if defined(ARDUINO_ARCH_SAM) || defined(ARDUINO_ARCH_ESP8266)
-    delay(10);
-#endif
+    Serial.println();
+    /*#if defined(ARDUINO_ARCH_SAM) || defined(ARDUINO_ARCH_ESP8266)
+        delay(10);
+      #endif */
     longTest();
+    Serial.println();
     floatTest();
+    Serial.println();
     structTest();
+    Serial.println();
     arrayTest();
+    Serial.println();
     stringTest();
 
-#if !defined(ARDUINO_ARCH_SAM) || !defined(ARDUINO_ARCH_ESP8266)
-    Serial.println();
-    powerDownTest();
-    powerUpTest();
-#endif
+    /* #if !defined(ARDUINO_ARCH_SAM) || !defined(ARDUINO_ARCH_ESP8266)
+        Serial.println();
+        powerDownTest();
+        powerUpTest();
+      #endif */
     printLine();
     if (!flash.functionRunTime()) {
       Serial.println(F("To see function runtimes ncomment RUNDIAGNOSTIC in SPIMemory.h."));
